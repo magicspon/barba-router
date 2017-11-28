da910ab86d717c2a241d2ff3f0a93549f52193b0
Nico Prat <nico@pamplemousse.com>
2017-11-28 16:25:37 +0100

    Add `getParams` object to `from` and `to` parameters
    Using https://stackoverflow.com/a/7090123/758728 as `searchToObject` util function.

--- src/js/core/utils/index.js
+++ src/js/core/utils/index.js
@@ -148,3 +148,19 @@ export const getNodeFromResp = (input, target) => {
 	wrapper.innerHTML = input
 	return wrapper.querySelector(target)
 }
+
+export const searchToObject = (search) => {
+  var pairs = search.substring(1).split("&"),
+    obj = {},
+    pair,
+    i;
+
+  for ( i in pairs ) {
+    if ( pairs[i] === "" ) continue;
+
+    pair = pairs[i].split("=");
+    obj[ decodeURIComponent( pair[0] ) ] = decodeURIComponent( pair[1] );
+  }
+
+  return obj;
+}
